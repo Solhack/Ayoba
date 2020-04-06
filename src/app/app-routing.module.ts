9a3d9ab0bd07be5ff2e4d0c24b7e172d35ce0a07
Nchaupe Solomon <Nchaupe.Solomon@sita.co.za>
2020-04-06 09:41:05 +0200

    13th commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { WelcomePage } from './welcome/welcome.page';
-import { Z_FULL_FLUSH } from 'zlib';
+
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,31 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePage } from './welcome/welcome.page';
+import { LoginPage } from './login/login.page';
+import { RegisterPage } from './register/register.page';
+import { TabsPage } from './tabs/tabs.page';
 
 
 @NgModule({
-  declarations: [AppComponent,
-  WelcomePage ],
-  entryComponents: [WelcomePage],
+  declarations: [
+  AppComponent,
+  WelcomePage,
+  LoginPage,
+  RegisterPage,
+  TabsPage
+],
+  entryComponents: [
+    WelcomePage,
+    LoginPage,
+    RegisterPage,
+    TabsPage
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, WelcomePage]
 })
 export class AppModule {}
